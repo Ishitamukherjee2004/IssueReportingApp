2908b45a1073bf7afd5ba0bb295f77ac583def11
Ishita Mukherjee <ishitam299@gmail.com>
2025-02-27 16:32:59 +0530

    Error solved

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,8 +17,8 @@ export default function Navbar() {
                 <div className="h-9 w-9 rounded-xl bg-gradient-to-br from-sky-400 to-blue-600 flex items-center justify-center">
                 <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 text-white">
                   <path 
-                  stroke-linecap="round" 
-                  stroke-linejoin="round" 
+                  strokeLinecap="round" 
+                  strokeLinejoin="round" 
                   strokeWidth={2}
                   d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
                 </svg>
